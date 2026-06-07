47472e3882f46f9ca353a29f5a7771f52c4151cc
Ismoil Abdujalilov <ismoilbek20050421@gmail.com>
2026-06-07 03:25:10 -0700

    feat: Addin an icon button to a header

--- src/app/meals/[mealId].tsx
+++ src/app/meals/[mealId].tsx
@@ -2,13 +2,35 @@ import List from "@/components/MealDetail/List";
 import SubTitle from "@/components/MealDetail/SubTitle";
 import MealDetails from "@/components/MealDetails";
 import { MEALS } from "@/data/dummy-data";
+import { useNavigation } from "expo-router";
 import { useLocalSearchParams } from "expo-router/build/hooks";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useLayoutEffect } from "react";
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const MealDetailScreen = () => {
+  const navigation = useNavigation();
   const { mealId } = useLocalSearchParams();
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  const headerButtonPressHandler = () => {
+    console.log("Pressed!");
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return <Button title="Tap me!" onPress={headerButtonPressHandler} />;
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
 
   return (
     <ScrollView style={styles.rootContainer}>
